a5a3485bc3898c2f1c03aaa643d9b10191035245
Christian Loose <christian.loose@hamburg.de>
2020-12-15 20:10:14 +0100

    Chapter 10 - More patterns
    Generalize patterns and add more implementations

--- features/steps/patterns_steps.py
+++ features/steps/patterns_steps.py
@@ -1,10 +1,19 @@
 from behave import given, when, then  # pylint: disable=no-name-in-module
 from asserts import assert_tuple
 from tuples import color, point
-from patterns import StripePattern
+from patterns import Pattern, StripePattern, GradientPattern, RingPattern, CheckersPattern
+from matrix import identity_matrix
 from transformations import scaling, translation
 
 COLORS = {'black': color(0, 0, 0), 'white': color(1, 1, 1)}
+
+
+class TestPattern(Pattern):
+    def __init__(self):
+        super().__init__()
+
+    def pattern_at(self, point):
+        return color(point[0], point[1], point[2])
 
 
 @given(u'black <- color(0, 0, 0)')
@@ -17,9 +26,29 @@ def step_create_color_white(context):
     context.white = color(1, 1, 1)
 
 
+@given(u'pattern <- test_pattern()')
+def step_create_test_pattern_pattern(context):
+    context.pattern = TestPattern()
+
+
 @given(u'pattern <- stripe_pattern(white, black)')
 def step_create_stripe_pattern_pattern(context):
     context.pattern = StripePattern(context.white, context.black)
+
+
+@given(u'pattern <- gradient_pattern(white, black)')
+def step_create_gradient_pattern_pattern(context):
+    context.pattern = GradientPattern(context.white, context.black)
+
+
+@given(u'pattern <- ring_pattern(white, black)')
+def step_create_ring_pattern_pattern(context):
+    context.pattern = RingPattern(context.white, context.black)
+
+
+@given(u'pattern <- checkers_pattern(white, black)')
+def step_create_checkers_pattern_pattern(context):
+    context.pattern = CheckersPattern(context.white, context.black)
 
 
 @given(u'set_transform(s, scaling({x:g}, {y:g}, {z:g}))')
@@ -37,8 +66,8 @@ def step_set_transform_of_pattern_to_translation_matrix(context, x, y, z):
     context.pattern.set_transform(translation(x, y, z))
 
 
-@when(u'c <- stripe_at_object(pattern, s, point({x:g}, {y:g}, {z:g}))')
-def step_set_c_to_color_of_stripe_at_point(context, x, y, z):
+@when(u'c <- pattern_at_shape(pattern, s, point({x:g}, {y:g}, {z:g}))')
+def step_set_c_to_color_of_pattern_at_point(context, x, y, z):
     context.c = context.pattern.pattern_at_shape(context.s, point(x, y, z))
 
 
@@ -52,14 +81,35 @@ def step_assert_color_b_of_pattern_equals_black(context):
     assert_tuple(context.pattern.color_b, context.black)
 
 
-@then(u'stripe_at(pattern, point({x:g}, {y:g}, {z:g})) = {expected_color}')
-def step_assert_pattern_at_point_equals_color(context, x, y, z,
-                                              expected_color):
+@then(u'pattern_at(pattern, point({x:g}, {y:g}, {z:g})) = "{expected_color}"')
+def step_assert_pattern_at_point_equals_constant_color(context, x, y, z,
+                                                       expected_color):
     actual = context.pattern.pattern_at(point(x, y, z))
     expected = COLORS.get(expected_color)
+    assert_tuple(actual, expected)
+
+
+@then(
+    u'pattern_at(pattern, point({x:g}, {y:g}, {z:g})) = color({red:g}, {green:g}, {blue:g})'
+)
+def step_assert_pattern_at_point_equals_color(context, x, y, z, red, green,
+                                              blue):
+    actual = context.pattern.pattern_at(point(x, y, z))
+    expected = color(red, green, blue)
     assert_tuple(actual, expected)
 
 
 @then(u'c = white')
 def step_assert_color_c_equals_white(context):
     assert_tuple(context.c, context.white)
+
+
+@then(u'pattern.transform = identity_matrix')
+def step_assert_transform_of_pattern_equals_identity_matrix(context):
+    assert context.pattern.transform() == identity_matrix()
+
+
+@then(u'pattern.transform = translation({x:g}, {y:g}, {z:g})')
+def step_assert_transform_of_pattern_equals_translation_matrix(
+    context, x, y, z):
+    assert context.pattern.transform() == translation(x, y, z)
--- patterns.py
+++ patterns.py
@@ -1,13 +1,11 @@
-from math import floor
+from math import floor, sqrt
+from tuples import color, add, subtract, multiply
 from matrix import identity_matrix, multiply_tuple, inverse
 
 
-class StripePattern:
-    """"""
-    def __init__(self, color_a, color_b):
-        """"""
-        self.color_a = color_a
-        self.color_b = color_b
+class Pattern:
+    """Abstract base class for all patterns."""
+    def __init__(self):
         self.__transform = identity_matrix()
         self.__inverse_transform = identity_matrix()
 
@@ -22,6 +20,24 @@ class StripePattern:
     def inverse_transform(self):
         return self.__inverse_transform
 
+    def pattern_at_shape(self, shape, point):
+        """"""
+        shape_point = multiply_tuple(shape.inverse_transform(), point)
+        pattern_point = multiply_tuple(self.__inverse_transform, shape_point)
+        return self.pattern_at(pattern_point)
+
+    def pattern_at(self, point):
+        raise NotImplementedError('subclass must override pattern_at')
+
+
+class StripePattern(Pattern):
+    """"""
+    def __init__(self, color_a, color_b):
+        """"""
+        super().__init__()
+        self.color_a = color_a
+        self.color_b = color_b
+
     def pattern_at(self, point):
         """"""
         if floor(point[0]) % 2 == 0.0:
@@ -29,8 +45,48 @@ class StripePattern:
 
         return self.color_b
 
-    def pattern_at_shape(self, shape, point):
+
+class GradientPattern(Pattern):
+    """"""
+    def __init__(self, color_a, color_b):
         """"""
-        shape_point = multiply_tuple(shape.inverse_transform(), point)
-        pattern_point = multiply_tuple(self.__inverse_transform, shape_point)
-        return self.pattern_at(pattern_point)
+        super().__init__()
+        self.color_a = color_a
+        self.color_b = color_b
+
+    def pattern_at(self, point):
+        distance = subtract(self.color_b, self.color_a)
+        fraction = point[0] - floor(point[0])
+        return add(self.color_a, multiply(distance, fraction))
+
+
+class RingPattern(Pattern):
+    """"""
+    def __init__(self, color_a, color_b):
+        """"""
+        super().__init__()
+        self.color_a = color_a
+        self.color_b = color_b
+
+    def pattern_at(self, point):
+        """"""
+        if floor(sqrt(point[0]**2 + point[2]**2)) % 2 == 0.0:
+            return self.color_a
+
+        return self.color_b
+
+
+class CheckersPattern(Pattern):
+    """"""
+    def __init__(self, color_a, color_b):
+        """"""
+        super().__init__()
+        self.color_a = color_a
+        self.color_b = color_b
+
+    def pattern_at(self, point):
+        """"""
+        if (floor(point[0]) + floor(point[1]) + floor(point[2])) % 2 == 0.0:
+            return self.color_a
+
+        return self.color_b
